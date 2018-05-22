1764122e4223c87f430d05f302b974130934ce97
root <meryll.dindin@student.ecp.fr>
2018-05-22 15:00:10 +0900

    Debug

--- package/models.py
+++ package/models.py
@@ -103,31 +103,28 @@ class DL_Model:
     def add_CONV2D(self, inp, callback):
 
         # Build model
-        mod = Reshape((1, inp._keras_shape[1], inp._keras_shape[2]))(inp)
+        shp = (1, inp._keras_shape[1], inp._keras_shape[2])
+        mod = Reshape(shp)(inp)
         mod = Convolution2D(64, (inp._keras_shape[1], 30), 
-                            data_format='channels_first',
-                            bias_regularizer=regularizers.l2(0.01))(mod)
-        mod = MaxPooling2D(pool_size=(1, 2), data_format='channels_first')(mod)
+                            data_format='channels_first')(mod)
+        mod = MaxPooling2D(pool_size=(1, 2), 
+                           data_format='channels_first')(mod)
         mod = BatchNormalization(axis=1)(mod)
-        mod = Activation('selu')(mod)
+        mod = Activation('relu')(mod)
         mod = AdaptiveDropout(callback.prb, callback)(mod)
-        mod = Convolution2D(128, (1, 15), 
-                            data_format='channels_first',
-                            bias_regularizer=regularizers.l2(0.01))(mod)
+        mod = Convolution2D(128, (1, 15))(mod)
         mod = BatchNormalization(axis=1)(mod)
-        mod = Activation('selu')(mod)
+        mod = Activation('relu')(mod)
         mod = AdaptiveDropout(callback.prb, callback)(mod)
         mod = GlobalAveragePooling2D()(mod)
         # Rework through dense network
-        mod = Dense(mod._keras_shape[1],
-                    bias_regularizer=regularizers.l2(0.01))(mod)
+        mod = Dense(mod._keras_shape[1])(mod)
         mod = BatchNormalization()(mod)
-        mod = Activation('selu')(mod)
+        mod = Activation('relu')(mod)
         mod = AdaptiveDropout(callback.prb, callback)(mod)
-        mod = Dense(mod._keras_shape[1] // 2,
-                    bias_regularizer=regularizers.l2(0.01))(mod)
+        mod = Dense(mod._keras_shape[1] // 2)(mod)
         mod = BatchNormalization()(mod)
-        mod = Activation('selu')(mod)
+        mod = Activation('relu')(mod)
 
         # Add layers to the model
         self.inp.append(inp)
@@ -140,28 +137,26 @@ class DL_Model:
 
         # Build the selected model
         mod = Reshape((inp._keras_shape[1], 1))(inp)
-        mod = Conv1D(64, 30,
-                     bias_regularizer=regularizers.l2(0.01))(mod)
+        mod = Conv1D(64, 30)(mod)
+        mod = Conv1D(64, 30)(mod)
         mod = MaxPooling1D(pool_size=2)(mod)
         mod = BatchNormalization()(mod)
-        mod = Activation('selu')(mod)
+        mod = Activation('relu')(mod)
         mod = AdaptiveDropout(callback.prb, callback)(mod)
-        mod = Conv1D(128, 15,
-                     bias_regularizer=regularizers.l2(0.01))(mod)
+        mod = Conv1D(128, 15)(mod)
+        mod = Conv1D(128, 15)(mod)
         mod = BatchNormalization()(mod)
-        mod = Activation('selu')(mod)
+        mod = Activation('relu')(mod)
         mod = AdaptiveDropout(callback.prb, callback)(mod)
         mod = GlobalMaxPooling1D()(mod)
         # Rework through dense network
-        mod = Dense(mod._keras_shape[1],
-                    bias_regularizer=regularizers.l2(0.01))(mod)
+        mod = Dense(mod._keras_shape[1])(mod)
         mod = BatchNormalization()(mod)
-        mod = Activation('selu')(mod)
+        mod = Activation('relu')(mod)
         mod = AdaptiveDropout(callback.prb, callback)(mod)
-        mod = Dense(mod._keras_shape[1] // 2,
-                    bias_regularizer=regularizers.l2(0.01))(mod)
+        mod = Dense(mod._keras_shape[1] // 2)(mod)
         mod = BatchNormalization()(mod)
-        mod = Activation('selu')(mod)
+        mod = Activation('relu')(mod)
 
         # Add model to main model
         self.inp.append(inp)
@@ -173,15 +168,13 @@ class DL_Model:
     def add_LDENSE(self, inp, callback):
 
         # Build the model
-        mod = Dense(inp._keras_shape[1], 
-                    bias_regularizer=regularizers.l2(0.01))(inp)
+        mod = Dense(inp._keras_shape[1])(inp)
         mod = BatchNormalization()(mod)
-        mod = Activation('selu')(mod)
+        mod = Activation('relu')(mod)
         mod = AdaptiveDropout(callback.prb, callback)(mod)
-        mod = Dense(mod._keras_shape[1] // 2,
-                    bias_regularizer=regularizers.l2(0.01))(mod)
+        mod = Dense(mod._keras_shape[1] // 2)(mod)
         mod = BatchNormalization()(mod)
-        mod = Activation('selu')(mod)
+        mod = Activation('relu')(mod)
 
         # Add layers to model
         self.inp.append(inp)
@@ -238,11 +231,10 @@ class DL_Model:
         # Defines the learning tail
         tails = np.linspace(2*self.n_c, 2*model._keras_shape[1], num=n_tail)
         for idx in range(n_tail):
-            model = Dense(int(tails[n_tail - 1 - idx]),
-                          bias_regularizer=regularizers.l2(0.01))(model)
+            model = Dense(int(tails[n_tail - 1 - idx]))(model)
             model = BatchNormalization()(model)
             model = Activation('relu')(model)
-            model = AdaptiveDropout(self.drp.prb, self.drp)(model)
+            model = Dropout(self.drp.prb / 2.0)(model)
 
         # Last layer for probabilities
         model = Dense(self.n_c, activation='softmax')(model)
