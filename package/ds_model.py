5a9112fee1dbe57371866007386c20509fd0a79a
root <meryll.dindin@student.ecp.fr>
2018-06-07 15:35:40 +0900

    New version for weights

--- package/ds_model.py
+++ package/ds_model.py
@@ -18,10 +18,10 @@ class DS_Model:
         self.pth = input_db
         self.cls = channel
         # Output definition
-        if marker: self.spc = './models/SPC_{}_{}.ks'.format(self.cls, marker)
-        else: self.spc = './models/SPC_{}.ks'.format(self.cls)
-        if marker: self.out = './models/DSN_{}_{}.ks'.format(self.cls, marker)
-        else: self.out = './models/DSN_{}.ks'.format(self.cls)
+        if marker: self.spc = './models/SPC_{}_{}.weights'.format(self.cls, marker)
+        else: self.spc = './models/SPC_{}.weights'.format(self.cls)
+        if marker: self.out = './models/DSN_{}_{}.weights'.format(self.cls, marker)
+        else: self.out = './models/DSN_{}.weights'.format(self.cls)
         if marker: self.his = './models/HIS_DSN_{}_{}.history'.format(self.cls, marker)
         else: self.his = './models/HIS_DSN_{}.history'.format(self.cls)
 
@@ -35,31 +35,18 @@ class DS_Model:
             self.train = dtb['eeg_{}_t'.format(self.cls)].value
             self.valid = dtb['eeg_{}_e'.format(self.cls)].value
             self.evals = dtb['eeg_{}_v'.format(self.cls)].value
-            self.sfreq = self.train.shape[1] / 30
+            self.sfreq = self.train.shape[1] // 30
 
         # Apply shuffling
         self.train, self.l_t = shuffle(self.train, self.l_t)
 
-    # Trains a first spatial representation of the EEG signals
-    # epochs refers to the maximum amount of epochs
-    # batch refers to the batch_size
-    # ini_dropout refers to the initialization of the annealing dropout
-    # decrease refers to the amount of epochs before annealation of the dropout
-    def train_spatial(self, epochs=100, batch=64, ini_dropout=0.5, decrease=50):
-
-        # Prepares the data, which will be balanced through oversampling
-        ros = RandomOverSampler()
-        vec, lab = shuffle(ros.fit_sample(self.train, self.l_t))
-
-        # Build the input
-        inp = Input(shape=(vec.shape[1], ))
-        inp = Reshape((inp._keras_shape[1], 1))(inp)
+    # Build the tensorflow graph for spatial learning
+    # inp refers to the tensorflow input
+    # drp refers to the annealing dropout callback
+    def build_spatial(self, inp, drp):
 
         # Layers arguments
         arg = {'kernel_initializer': 'he_normal'}
-        drp = DecreaseDropout(ini_dropout, decrease)
-        ear = EarlyStopping(monitor='loss', min_delta=1e-5, patience=10, verbose=0)
-        chk = ModelCheckpoint(self.spc, monitor='loss', save_best_only=True, save_weights_only=False)
 
         # Build the channel aimed at small patterns
         s_mod = Reshape((inp._keras_shape[1], 1))(inp)
@@ -78,11 +65,10 @@ class DS_Model:
         s_mod = PReLU()(s_mod)
         s_mod = BatchNormalization()(s_mod)
         s_mod = MaxPooling1D(pool_size=4, strides=4)(s_mod)
-        print('# Small feature map shape:', s_mod._keras_shape)
 
         # Build the channel aimed at longer patterns
         l_mod = Reshape((inp._keras_shape[1], 1))(inp)
-        l_mod = Conv1D(64, f_s*4, strides=13, **arg)(l_mod)
+        l_mod = Conv1D(64, self.sfreq*4, strides=13, **arg)(l_mod)
         l_mod = PReLU()(l_mod)
         l_mod = BatchNormalization()(l_mod)
         l_mod = MaxPooling1D(pool_size=4, strides=4)(l_mod)
@@ -97,7 +83,6 @@ class DS_Model:
         l_mod = PReLU()(l_mod)
         l_mod = BatchNormalization()(l_mod)
         l_mod = MaxPooling1D(pool_size=2, strides=2)(l_mod)
-        print('# Large feature map shape:', l_mod._keras_shape)
 
         # Concatenate both channels
         space = concatenate([s_mod, l_mod])
@@ -107,7 +92,30 @@ class DS_Model:
         model = GlobalMaxPooling1D()(space)
         model = Dense(self.n_c, activation='softmax', **arg)(model)
 
+        return model
+
+    # Trains a first spatial representation of the EEG signals
+    # epochs refers to the maximum amount of epochs
+    # batch refers to the batch_size
+    # ini_dropout refers to the initialization of the annealing dropout
+    # decrease refers to the amount of epochs before annealation of the dropout
+    def train_spatial(self, epochs=100, batch=64, ini_dropout=0.5, decrease=50):
+
+        # Prepares the data, which will be balanced through oversampling
+        ros = RandomOverSampler()
+        vec, lab = shuffle(*ros.fit_sample(self.train, self.l_t))
+
+        # Layers arguments
+        drp = DecreaseDropout(ini_dropout, decrease)
+        ear = EarlyStopping(monitor='loss', min_delta=1e-5, patience=10, verbose=0)
+        chk = ModelCheckpoint(self.spc, monitor='loss', save_best_only=True, save_weights_only=True)
+
+        # Build the model
+        inp = Input(shape=(vec.shape[1], ))
+        model = self.build_spatial(inp, drp)
+
         # Launch the learning process
+        model = Model(inputs=inp, outputs=model)
         optim = Adadelta(clipnorm=0.1)
         model.compile(metrics=['accuracy'], loss='categorical_crossentropy', optimizer=optim)
         model.fit(vec, np_utils.to_categorical(lab), verbose=1, epochs=epochs,
@@ -115,34 +123,27 @@ class DS_Model:
                   class_weight=class_weight(lab.ravel()), batch_size=batch)
 
         # Memory efficiency
-        del ros, vec, lab, inp, arg, drp, ear, chk, s_mod, l_mod, space, model, optim
+        del ros, vec, lab, inp, drp, ear, chk, model, optim
     
-    # Ends the training by merging the spatial features into a temporal dimension
-    # epochs refers to the maximum amount of epochs
-    # batch refers to the batch_size
-    # ini_dropout refers to the initialization of the annealing dropout
-    # decrease refers to the amount of epochs before annealation of the dropout
-    def train_temporal(self, epochs=100, batch=64, ini_dropout=0.5, decrease=50):
+    # Build the tensorflow graph for temporal learning
+    # inp refers to the tensorflow input
+    # drp refers to the annealing dropout callback
+    def build_temporal(self, inp, drp):
 
-        # Build the input
-        inp = Input(shape=(vec.shape[1], ))
-        inp = Reshape((inp._keras_shape[1], 1))(inp)
-
-        # Load the spatial model
-        space = load_model(self.spc)
-        space = Model(inputs=space.input, outputs=space.layers[-3].output)
-        for layer in space.layers: layer.trainable = False
-
-        # Layers arguments
+        # Layers argument
         arg = {'kernel_initializer': 'he_normal',
                'kernel_constraint': max_norm(5.0),
                'kernel_regularizer': regularizers.l2(1e-3)}
-        drp = DecreaseDropout(ini_dropout, decrease)
-        ear = EarlyStopping(monitor='val_loss', min_delta=1e-5, patience=10, verbose=0)
-        chk = ModelCheckpoint(self.spc, monitor='val_loss', save_best_only=True, save_weights_only=False)
 
-        model = space(inp)
-        print('# Convolutional feature space:', model._keras_shape)
+        # Load the spatial model
+        space = self.build_spatial(inp, drp)
+        space.load_weights(self.spc)
+        space = Model(inputs=space.input, outputs=space.layers[-3].output)
+        for layer in space.layers: layer.trainable = False
+        
+        # Intialize the new model
+        model = Reshape((inp._keras_shape[1], 1))(inp)
+        model = space(model)
 
         tempo = Bidirectional(LSTM(256, return_sequences=True, **arg))(model)
         tempo = BatchNormalization()(tempo)
@@ -152,7 +153,6 @@ class DS_Model:
         tempo = BatchNormalization()(tempo)
         tempo = PReLU()(tempo)
         tempo = AdaptiveDropout(drp.prb, drp)(tempo)
-        print('# Temporal feature map shape:', tempo._keras_shape)
 
         # Residual fully connected layer
         resid = GlobalMaxPooling1D()(model)
@@ -161,7 +161,6 @@ class DS_Model:
         resid = PReLU()(resid)
 
         model = Add()([tempo, resid])
-        print('# Intermediate map:', model._keras_shape)
         model = Dense(model._keras_shape[1] // 2, **arg)(model)
         model = BatchNormalization()(model)
         model = PReLU()(model)
@@ -171,6 +170,24 @@ class DS_Model:
         model = PReLU()(model)
         model = AdaptiveDropout(drp.prb, drp)(model)
         model = Dense(self.n_c, activation='softmax', **arg)(model)
+
+        return model        
+
+    # Ends the training by merging the spatial features into a temporal dimension
+    # epochs refers to the maximum amount of epochs
+    # batch refers to the batch_size
+    # ini_dropout refers to the initialization of the annealing dropout
+    # decrease refers to the amount of epochs before annealation of the dropout
+    def train_temporal(self, epochs=100, batch=64, ini_dropout=0.5, decrease=50):
+
+        # Layers arguments
+        drp = DecreaseDropout(ini_dropout, decrease)
+        ear = EarlyStopping(monitor='val_loss', min_delta=1e-5, patience=10, verbose=0)
+        chk = ModelCheckpoint(self.spc, monitor='val_loss', save_best_only=True, save_weights_only=True)
+
+        # Build the input
+        inp = Input(shape=(self.train.shape[1], ))
+        model = self.build_temporal(inp, drp)
 
         # Launch the learning process
         model = Model(inputs=inp, outputs=model)
