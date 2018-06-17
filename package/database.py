f1c7face3aea50e8b9177e15669a61f22e8a952b
root <meryll.dindin@student.ecp.fr>
2018-06-18 08:07:21 +0900

    Fix on rescale

--- package/database.py
+++ package/database.py
@@ -160,25 +160,25 @@ class Database:
 
         lst = ['norm_acc', 'po_ir', 'eeg_1', 'eeg_2', 'eeg_3', 'eeg_4']
 
-        # for pth in [self.train_out, self.valid_out]:
+        for pth in [self.train_out, self.valid_out]:
 
-        #     res = []
-        #     # Iterates over the keys
-        #     for key in tqdm.tqdm(lst):
+            res = []
+            # Iterates over the keys
+            for key in tqdm.tqdm(lst):
 
-        #         # Load the corresponding values
-        #         with h5py.File(pth, 'r') as dtb: val = dtb[key].value
-        #         # Multiprocessed computation
-        #         pol = multiprocessing.Pool(processes=multiprocessing.cpu_count())
-        #         res.append(np.asarray(pol.map(compute_features, val)))
-        #         pol.close()
-        #         pol.join()
+                # Load the corresponding values
+                with h5py.File(pth, 'r') as dtb: val = dtb[key].value
+                # Multiprocessed computation
+                pol = multiprocessing.Pool(processes=multiprocessing.cpu_count())
+                res.append(np.asarray(pol.map(compute_features, val)))
+                pol.close()
+                pol.join()
 
-        #     # Serialize the output
-        #     with h5py.File(pth, 'a') as dtb:
-        #         if dtb.get('fea'): del dtb['fea']
-        #         dtb.create_dataset('fea', data=np.hstack(tuple(res)))
-        #         del res
+            # Serialize the output
+            with h5py.File(pth, 'a') as dtb:
+                if dtb.get('fea'): del dtb['fea']
+                dtb.create_dataset('fea', data=np.hstack(tuple(res)))
+                del res
 
         # Build the features relative to their PCA reduction
         train_pca, valid_pca = [], []
@@ -223,7 +223,6 @@ class Database:
                    'norm_acc', 'norm_eeg', 
                    'eeg_1', 'eeg_2', 'eeg_3', 'eeg_4', 
                    'po_r', 'po_ir',
-                   'wav_1', 'wav_2', 'wav_3', 'wav_4',
                    'bup_1', 'bup_2', 'bup_3', 'bup_4',
                    'bdw_1', 'bdw_2', 'bdw_3', 'bdw_4']
             env = ['eeg_1', 'eeg_2', 'eeg_3', 'eeg_4', 'po_r', 'po_ir']
