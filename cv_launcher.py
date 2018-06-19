f53015d4d37d5e07af00e4e172be8b57ddcd5535
root <meryll.dindin@student.ecp.fr>
2018-06-19 12:05:15 +0900

    Fix on type of argument

--- cv_launcher.py
+++ cv_launcher.py
@@ -14,7 +14,7 @@ if __name__ == '__main__':
     prs = argparse.ArgumentParser()
     # Mandatory arguments
     prs.add_argument('-n', '--name', help='Type of model to use', type=str, default='XGB')
-    prs.add_argument('-f', '--folds', help='Number of folds for cross-validation', type=str, default=7)
+    prs.add_argument('-f', '--folds', help='Number of folds for cross-validation', type=int, default=7)
     prs.add_argument('-t', '--threads', help='Amount of threads', type=int, default=multiprocessing.cpu_count())
     prs.add_argument('-l', '--log_file', help='Where to write out the intermediate scores', type=str, default='./nohup/CV_SCORING.log')
     # Parse the arguments
