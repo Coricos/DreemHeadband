30fb4b2a5c269eeb3924d0fc5ed1783e67296030
root <meryll.dindin@student.ecp.fr>
2018-06-22 18:32:32 +0900

    Evolution of the rescaling

--- package/database.py
+++ package/database.py
@@ -249,7 +249,7 @@ class Database:
         with h5py.File(self.train_out, 'r') as dtb:
             res = ['norm_acc', 'norm_eeg', 'acc_x', 'acc_y', 'acc_z', 'eeg_1', 'eeg_2', 'eeg_3', 'eeg_4', 'po_r', 'po_ir']
             unt = ['bup_1', 'bup_2', 'bup_3', 'bup_4']
-            ldc = ['l_0_1', 'l_0_2', 'l_0_3', 'l_0_4', 'l_1_1', 'l_1_2', 'l_1,3', 'l_1_4']
+            ldc = ['l_0_1', 'l_0_2', 'l_0_3', 'l_0_4', 'l_1_1', 'l_1_2', 'l_1_3', 'l_1_4']
             oth = [key for key in list(dtb.keys()) if key not in res + unt + ldc + ['lab']]
 
         # Transfer the labels from DTS to SCA
