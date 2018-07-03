3dfdf0743035c4e83c35f70f72c6e070c187f5e1
root <meryll.dindin@student.ecp.fr>
2018-07-03 14:55:36 +0900

    Minor fix

--- dl_launcher.py
+++ dl_launcher.py
@@ -10,6 +10,8 @@ from package.dl_model import *
 
 if __name__ == '__main__':
 
+	warnings.simplefilter('ignore')
+
     # Initialize the arguments
     prs = argparse.ArgumentParser()
     # Mandatory arguments
@@ -21,9 +23,13 @@ if __name__ == '__main__':
     dic = generate_channels(prs.channels)
 
     # Build a new database
+    print('\n# Database construction:')
+    time.sleep(0.5)
     Database().preprocess('./dataset/DTB_Headband.h5', test=0.33)
 
     # Build and launch the corresponding DL model
+    print('\n# Model launching:')
+    time.sleep(0.5)
     mod = DL_Model('./dataset/DTB_Headband.h5', dic)
     mod.learn()
     mod.write_to_file()
