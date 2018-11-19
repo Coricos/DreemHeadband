ba85dd67c950593b138b69082f6ae67f2ca45ab2
Coricos <meryll.dindin@student.ecp.fr>
2018-11-19 13:46:09 -0800

    Anomaly detection and specific feature construction

---
+++ package/detection.py
@@ -0,0 +1,129 @@
+# DINDIN Meryll
+# Nov 16th, 2018
+# Dreem Headband Sleep Phases Classification Challenge
+
+try: from package.toolbox import *
+except: from toolbox import *
+
+# Develops a class relative to profile extraction
+
+class Profiles:
+
+    def __init__(self, labels):
+
+        try: self.lab = labels.values.ravel()
+        except: self.lab = labels.ravel()
+
+    def ratios(self):
+
+        prp = [len(np.where(self.lab == idx)[0]) / len(self.lab) for idx in np.unique(self.lab)]
+
+        plt.figure(figsize=(18,4))
+        plt.bar(np.arange(len(prp)), prp, color='salmon')
+        plt.xlabel('Sleep Phase')
+        plt.ylabel('Relative Ratio')
+        plt.show()
+
+    def build_profiles(self):
+
+        lst = [list(grp) for k, grp in groupby(self.lab)]
+        s_p = np.asarray([ele[0] for ele in lst])
+        sms = np.asarray([len(ele) for ele in lst])
+        ind = np.where(s_p == 0)[0]
+
+        idx = np.asarray(sorted(list(set(np.where(sms > 10)[0]) & set(ind))))
+        mea = np.mean(np.diff(np.cumsum(sms)[idx]) * 30) / 60 / 60
+        print('# Mean Duration of Night per Person: {} hours'.format(np.round(mea, 1)))
+
+        prf, mkr = [], []
+        for pat in range(len(idx)-1):
+            dec = np.asarray([0] + list(np.cumsum(sms)))
+            beg, end = max(0, dec[idx[pat]] + sms[idx[pat]] // 2), min(len(self.lab), dec[idx[pat+1]] + sms[idx[pat+1]] // 2)
+            mkr.append((beg, end))
+            prf.append(self.lab[beg:end])
+            
+        return prf, mkr
+
+    def display_profile(self, profile):
+
+        plt.figure(figsize=(18,4))
+        ind = np.arange(len(profile))
+        for idx in np.unique(profile):
+            plt.hlines(idx, 0, len(profile), linestyles='--', lw=1.0)
+            ext = np.where(profile == idx)[0]
+            plt.bar(ind[ext], 0.5, bottom=profile[ext]-0.25, width=1.2)
+        plt.ylim([-0.5, 4.5])
+        plt.xlabel('Time')
+        plt.ylabel('Sleep Phase')
+        plt.show()
+
+    def learn(self, profiles, save='./models/lstm_labels.ks', timesteps=30, test_size=0.25, epochs=100):
+
+        vec = []
+
+        # Decompose the profiles into learning timesteps
+        for profile in profiles:
+            m_i = np.arange(timesteps+1)*(0+1)
+            m_j = np.arange(np.max(profile.shape[0]-(timesteps)*(0+1), 0))
+            vec.append(profile[m_i + m_j.reshape(-1,1)])
+        # Categorical transformation
+        vec = np_utils.to_categorical(np.vstack(tuple(vec)))
+        x,y = vec[:,:timesteps,:], vec[:,-1,:]
+        # Train test separation
+        x_t, x_v, y_t, y_v = train_test_split(x, y, test_size=test_size, shuffle=True)
+
+        # Build the LSTM model
+        model = Sequential()
+        model.add(LSTM(30, input_shape=(x_t.shape[1], x_t.shape[2]), return_sequences=True))
+        model.add(Dropout(0.5))
+        model.add(LSTM(30, return_sequences=False))
+        model.add(Dropout(0.5))
+        model.add(Dense(5, activation='softmax'))
+        model.compile(loss='categorical_crossentropy', optimizer='adadelta', metrics=['accuracy'])
+
+        # Defines the callbacks
+        arg = {'patience': 5, 'verbose': 0}
+        early = EarlyStopping(monitor='val_acc', min_delta=1e-5, **arg)
+        arg = {'save_best_only': True, 'save_weights_only': True}
+        check = ModelCheckpoint(save, monitor='val_acc', **arg)
+
+        # Launch the training
+        model.fit(x_t, y_t, epochs=epochs, batch_size=32, shuffle=True, 
+                  callbacks=[early, check], validation_data=(x_v, y_v))
+
+# Detect anomalies
+
+class Anomaly:
+
+    def __init__(self, h5_train='./dataset/train.h5', h5_valid='./dataset/valid.h5'):
+
+        e_key = ['eeg_{}'.format(dim) for dim in range(1, 5)]
+
+        with h5py.File(h5_train, 'r') as dtb:
+            eeg_t = np.asarray([dtb[key].value for key in e_key])
+            nrm_t = np.sqrt(np.sum(np.square(eeg_t), axis=0))
+            del eeg_t
+
+        with h5py.File(h5_valid, 'r') as dtb:
+            eeg_v = np.asarray([dtb[key].value for key in e_key])
+            nrm_v = np.sqrt(np.sum(np.square(eeg_v), axis=0))
+            del eeg_v
+
+        self.nrm_s = np.vstack((nrm_t, nrm_v))
+        del nrm_t, nrm_v
+
+    def spot_outliers(self, update=True, save='./models/row_mask.npy', threshold=4):
+
+        msk = np.ones(len(self.nrm_s), dtype=bool)
+        # Build standard features for outliers spotting
+        m_x = np.max(self.nrm_s, axis=1)
+        grd = np.max(np.gradient(self.nrm_s, axis=1), axis=1)
+        auc = np.trapz(self.nrm_s, axis=1)
+        # Fill the mask
+        msk[outlier_from_median(m_x, threshold)] = False
+        msk[outlier_from_median(grd, threshold)] = False
+        msk[outlier_from_median(auc, threshold)] = False
+
+        np.save(save, msk)
+
+        return msk
---
+++ package/features.py
@@ -0,0 +1,194 @@
+# DINDIN Meryll
+# Nov 19th, 2018
+# Dreem Headband Sleep Phases Classification Challenge
+
+try: from package.toolbox import *
+except: from toolbox import *
+
+# Computes the pairwise distances between EEGs
+# signal refers to a 1D array
+
+def frequency_features(signal, brain=False):
+
+    res, f_s = [], int(len(signal)/30.0)
+
+    # Basic features
+    f,s = sg.periodogram(signal, fs=f_s)
+    res.append(f[s.argmax()])
+    res.append(np.max(s))
+    res.append(np.sum(s))
+    res.append(entropy(s))
+    
+    # Brain waves frequencies
+    if brain:
+
+        res.append(np.sum(s[np.where((f > 0.5) & (f < 3.0))[0]]))
+        res.append(np.sum(s[np.where((f > 3.0) & (f < 8.0))[0]]))
+        res.append(np.sum(s[np.where((f > 12.) & (f < 38.))[0]]))
+        res.append(np.sum(s[np.where((f > 38.) & (f < 42.))[0]]))
+
+        f,_,S = sg.spectrogram(signal, fs=f_s, return_onesided=True)
+        res += list(f[S.argmax(axis=0)])
+        psd = np.sum(S, axis=0)
+        res.append(np.mean(psd))
+        res.append(np.std(psd))
+        res.append(entropy(psd))
+
+        f,_,Z = sg.stft(signal, fs=f_s, window='hamming', nperseg=int(5*f_s), noverlap=int(0.75*5*f_s))
+        Z = np.abs(Z.T)
+        res += list(f[Z.argmax(axis=1)])
+        psd = np.sum(Z, axis=1)
+        res.append(np.mean(psd))
+        res.append(np.std(psd))
+        res.append(entropy(psd))
+
+    return res
+
+# Parameters of the autoregressive models
+# vec refers to a 1D array
+
+def ar_coefficients(vec):
+
+    nan = np.where(np.invert(np.isnan(vec)))[0]
+
+    mod = AR(vec[nan])
+    mod = mod.fit()
+    
+    return list(mod.params)
+
+# Defines the amount of crossing-overs
+# val refers to a 1D array
+
+def crossing_over(val):
+    
+    nan = np.where(np.invert(np.isnan(val)))[0]
+    
+    sgn = np.sign(val[nan])
+    sgn[sgn == 0] == -1
+
+    return len(np.where(np.diff(sgn))[0])
+
+# Defines the entropy of the signal
+# val refers to a 1D array
+
+def entropy(val):
+
+    nan = np.where(np.invert(np.isnan(val)))[0]
+    vec = val[nan]
+    
+    dta = np.round(vec, 5)
+    cnt = Counter()
+
+    for ele in dta: cnt[ele] += 1
+
+    pbs = [vec / len(dta) for vec in cnt.values()]
+    pbs = [prd for prd in pbs if prd > 0.0]
+
+    ent = 0.0
+    for prd in pbs:
+        ent -= prd * log(prd, 2.0)
+
+    return ent
+
+# Defines the wavelet features
+# val refers to a 1D array
+
+def wavelet_features(val):
+
+    res = []
+
+    cA_5, cD_5, cD_4, cD_3, _, _ = pywt.wavedec(val, 'db4', level=5)
+
+    for sig in [cA_5, cD_5, cD_4, cD_3]:
+        res += [np.min(sig), np.max(sig), np.sum(np.square(sig)), np.mean(sig), np.std(sig)]
+        
+    sgn = np.sign(val)
+    sgn = np.split(sgn, np.where(np.diff(sgn) != 0)[0]+1)
+    sgn = np.asarray([len(ele) for ele in sgn])
+    res += [np.nanmean(sgn), np.std(sgn)]
+    
+    sgn, ine = np.asarray([0] + list(sgn)), 0.0
+    for idx in range(len(sgn)-1):
+        ine += (sgn[idx+1] - sgn[idx])*np.trapz(np.abs(val[np.sum(sgn[:idx]):np.sum(sgn[:idx+1])]))
+    res.append(ine)
+
+    return res
+
+# Compute specific entropy features
+# val refers to a 1D array
+
+def neural_entropy_features(val):
+
+    warnings.simplefilter('ignore')
+
+    res, f_s = [], int(len(val) / 30.0)
+    
+    arg = {'lyap_r': False, 'lyap_e': True, 'sampen': False, 'multiscale': False}
+    tmp = neurokit.complexity(val, sampling_rate=f_s, **arg)
+    for key in sorted(list(tmp.keys())): res.append(tmp[key])
+    res.append(nolds.sampen(val))
+    res.append(nolds.lyap_r(val))
+
+    dif = np.diff(val)
+    dif = np.asarray([val[0]] + list(dif))
+    num = len(val)
+
+    m_2 = float(np.sum(dif ** 2)) / num
+    t_p = np.sum(np.square(val))
+    res.append(np.sqrt(m_2 / t_p))
+    
+    m_4 = 0.0
+    for idx in range(1, len(dif)):
+        m_4 += np.square(dif[idx] - dif[idx-1])
+    m_4 = m_4 / num
+    res.append(np.sqrt(m_4 * t_p / m_2 / m_2))
+
+    return np.asarray(res)
+
+# Defines the feature construction pipeline
+# val refers to a 1D array
+
+def stats_features(val):
+
+    def nested_stats(signal):
+        
+        res = []
+
+        nan = np.where(np.invert(np.isnan(signal)))[0]
+        
+        res.append(min(signal[nan]))
+        res.append(max(signal[nan]))
+        res.append(np.nanmean(signal[nan]))
+        res.append(np.nanstd(signal[nan]))
+        res.append(kurtosis(signal[nan]))
+        res.append(skew(signal[nan]))
+        res.append(entropy(signal[nan]))
+        
+        return res
+    
+    # Build the feature vector
+    res = nested_stats(val)
+    for per in [25, 50, 75]: res.append(np.percentile(val, per))
+
+    # Common features
+    tmp = seasonal_decompose(val, model='additive', freq=int(len(val)/30))
+    res += nested_stats(tmp.trend)
+    res.append(crossing_over(tmp.trend))
+    res += ar_coefficients(tmp.trend)
+    res += nested_stats(tmp.resid)
+    
+    return np.asarray(res)
+
+# General computation of features
+
+def compute_features(val, brain=False):
+
+    res = list(stats_features(val))
+    res += list(frequency_features(val, brain=brain))
+
+    if brain:
+
+        res += list(neural_entropy_features(val))
+        res += list(wavelet_features(val))
+
+    return np.asarray(res)
