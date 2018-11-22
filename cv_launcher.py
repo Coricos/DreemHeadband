df2d984aaa74a6439e0b9f4dcf9060c0514f94c6
Coricos <meryll.dindin@student.ecp.fr>
2018-11-22 11:31:40 -0800

    Build automated cv launcher

--- cv_launcher.py
+++ cv_launcher.py
@@ -4,8 +4,23 @@
 
 import argparse, warnings
 
-from package.ml_model import *
-from package.dl_model import *
+from package.detection import *
+from optiML import CrossClassification
+
+# Defines a split generator
+
+def split_generator(folds=10):
+
+    lab = pd.read_csv('./dataset/label.csv', sep=';', index_col=0)
+    msk = np.load('./models/row_mask.npy')[:len(lab)]
+    pro = Profiles(lab)
+    _,i = pro.build_profiles()
+
+    kfl = KFold(n_splits=folds, shuffle=True)
+    for _, ele in kfl.split(np.arange(len(i))):
+        val = np.zeros(len(lab), dtype=bool)
+        for idx in ele: val[i[idx][0]:i[idx][1]] = True
+        yield np.where(np.invert(val) & msk)[0], np.where(val)[0]
 
 # Main algorithm
 
@@ -14,26 +29,20 @@ if __name__ == '__main__':
     # Initialize the arguments
     prs = argparse.ArgumentParser()
     # Mandatory arguments
-    prs.add_argument('-o', '--objectif', help='Objectif of cross-validation', type=str, default='dl')
-    prs.add_argument('-n', '--name', help='Type of model to use', type=str, default='XGB')
-    prs.add_argument('-f', '--folds', help='Number of folds for cross-validation', type=int, default=7)
+    prs.add_argument('-m', '--model', help='Type of model to use', type=str, default='LGB')
+    prs.add_argument('-f', '--folds', help='Number of folds for cross-validation', type=int, default=5)
     prs.add_argument('-t', '--threads', help='Amount of threads', type=int, default=multiprocessing.cpu_count())
-    prs.add_argument('-p', '--output', help='Predictions file', type=str, default='./results/cMOD_{}.csv'.format(int(time.time())))
-    prs.add_argument('-c', '--channels', help='Channels Definition', nargs='*')
-    prs.add_argument('-x', '--max_iter', help='Maximum Iterations for hyperband', type=int, default=100)
-    prs.add_argument('-m', '--multi', help='Whether to apply multiprocessed hyperband or not', type=bool, default=False)
-    prs.add_argument('-l', '--log_file', help='Where to write out the intermediate scores', type=str, default='./models/CV_SCORING.log')
+    prs.add_argument('-i', '--max_iter', help='Maximum iterations', type=int, default=100)
+    prs.add_argument('-s', '--slurm', help='Whether to use slurm', type=bool, default=False)
     # Parse the arguments
     prs = prs.parse_args()
 
-    # Launch cross-validation for ml models
-    if prs.objectif == 'ml':
-        mod = CV_ML_Model('./dataset/sca_train.h5', k_fold=prs.folds, threads=prs.threads, mp=prs.multi)
-        mod.launch(prs.name, log_file=prs.log_file, max_iter=prs.max_iter)
-        mod.make_predictions('./dataset/sca_valid.h5', prs.name, scaler='./models/VTF_Selection.jb')
+    # Load the data relative to the problem
+    with h5py.File('./dataset/dts_train.h5', 'r') as dtb:
+        x_t, y_t = dtb['fea'].value, dtb['lab'].value
+    with h5py.File('./dataset/dts_valid.h5', 'r') as dtb:
+        x_v = dtb['fea'].value
 
-    # Launch cross-validation for dl models
-    if prs.objectif == 'dl':
-        dic = generate_channels(prs.channels)
-        mod = CV_DL_Model(dic, storage='./dataset')
-        mod.launch(out=prs.output, log_file=prs.log_file)
+    # Launch the model optimization
+    clf = CrossClassification(x_t, x_v, y_t, slurm=prs.slurm, threads=prs.threads)
+    clf.launch(split_generator(folds=prs.folds), metric='kap', model=prs.model, max_iter=prs.max_iter)
