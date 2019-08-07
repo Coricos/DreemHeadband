e325b22146feb36fae2976b24eafb09289b5eda3
Coricos <meryll.dindin@student.ecp.fr>
2019-08-06 22:32:42 -0700

    Shift towards challenger package usage

--- stimuler/imports.py
+++ stimuler/imports.py
@@ -2,23 +2,14 @@
 # Date:    05 August 2019
 # Project: DreemHeadband
 
-import pywt
 import h5py
-import nolds
 import argparse
 import warnings
 import numpy as np
 import pandas as pd
-import scipy.signal as sg
 
 from tqdm import tqdm
-from scipy.stats import skew
-from scipy.stats import kurtosis
-from collections import Counter
 from multiprocessing import cpu_count, Pool
-from statsmodels.tsa.ar_model import AR
-from concurrent.futures import ThreadPoolExecutor
-from statsmodels.tsa.seasonal import seasonal_decompose
 
 try: import seaborn as sns
 except: pass
---
+++ stimuler/optimizer.py
@@ -0,0 +1,5 @@
+# Author:  DINDIN Meryll
+# Date:    06 August 2019
+# Project: DreemHeadband
+
+
